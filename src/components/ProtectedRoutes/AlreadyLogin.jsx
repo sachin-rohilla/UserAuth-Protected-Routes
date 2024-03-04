d8dc79a63b37040d597980fc299d476321a27d93
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-04 23:48:48 +0530

    changes

--- src/components/ProtectedRoutes/AlreadyLogin.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../../Provider/AuthProvider";
-
-const AlreadyLogin = ({ children }) => {
-  const { token } = useContext(AuthContext);
-  return <div>{!token && children}</div>;
-};
-
-export default AlreadyLogin;
